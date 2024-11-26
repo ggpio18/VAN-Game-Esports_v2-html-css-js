42b908fdd643757c58db14ca68553883710eb4fa
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-11-26 11:55:01 +0800

    +=[header_fixed_js]

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,17 @@ for (let i = 0; i < searchTogglers.length; i++) {
     searchBox.classList.toggle("active");
   });
 }
+
+// header
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
